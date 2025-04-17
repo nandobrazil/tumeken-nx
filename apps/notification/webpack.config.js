9db9028e2f71abe4d67a1d6696c33b43ea4c5236
Fernando R. Conde <contato@frinfo.com.br>
2025-04-17 00:37:27 -0300

    fix project issues for start

--- apps/notification/webpack.config.js
+++ apps/notification/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../dist/notification'),
+    path: join(__dirname, '../../dist/apps/notification'),
   },
   plugins: [
     new NxAppWebpackPlugin({
